c178ddff33037a0480ec56e70eda663d4e6cb275
Thibault Zanini <thibaultzanini@gmail.com>
2019-05-30 18:54:05 -0400

    Remove old code

--- packages/calculators/lib/calculators.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = calculators;
-
-function calculators() {
-    // TODO
-}
--- packages/indicators/lib/indicators.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = indicators;
-
-function indicators() {
-    // TODO
-}
